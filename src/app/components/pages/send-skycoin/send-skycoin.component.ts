462e50c785da93591adbe7e73585225a4cdd1979
Unknown <xano3425@gmail.com>
2017-11-19 21:20:07 +0100

    Implement Send and prepare for prod

--- src/app/components/pages/send-skycoin/send-skycoin.component.ts
+++ src/app/components/pages/send-skycoin/send-skycoin.component.ts
@@ -16,40 +16,23 @@ export class SendSkycoinComponent implements OnInit {
   @ViewChild('button') button;
 
   form: FormGroup;
-  records = [];
   transactions = [];
 
   constructor(
     public formBuilder: FormBuilder,
     public walletService: WalletService,
-    private router: Router,
     private snackbar: MdSnackBar,
   ) {}
 
   ngOnInit() {
     this.initForm();
-    IntervalObservable
-      .create(2500)
-      .filter(() => !!this.transactions.length)
-      .flatMap(() => this.walletService.retrieveUpdatedTransactions(this.transactions))
-      .subscribe(transactions => this.records = transactions);
-    this.walletService.recent().subscribe(transactions => this.transactions = transactions);
-  }
-
-  onActivate(response) {
-    if (response.row && response.row.txid) {
-      this.router.navigate(['/history', response.row.txid]);
-    }
   }
 
   send() {
     this.button.setLoading();
-    const wallet_id = this.form.value.wallet.meta.filename;
-    this.walletService.sendSkycoin(wallet_id, this.form.value.address, this.form.value.amount * 1000000)
-      .delay(1000)
+    this.walletService.sendSkycoin(this.form.value.wallet, this.form.value.address, this.form.value.amount)
       .subscribe(
-        response => {
-          this.resetForm();
+        () => {
           this.button.setSuccess();
         },
         error => {
@@ -68,7 +51,7 @@ export class SendSkycoinComponent implements OnInit {
       amount: ['', [Validators.required, Validators.min(0), Validators.max(0)]],
     });
     this.form.controls['wallet'].valueChanges.subscribe(value => {
-      const balance = value && value.balance ? (value.balance / 1000000) : 0;
+      const balance = value && value.balance ? value.balance : 0;
       this.form.controls['amount'].setValidators([
         Validators.required,
         Validators.min(0),
