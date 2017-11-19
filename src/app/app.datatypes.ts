59cb71ee06289313fcccb8d9a8faf7abf28eb375
Unknown <xano3425@gmail.com>
2017-11-19 18:41:09 +0100

    Add datatypes for outputs processing

--- src/app/app.datatypes.ts
+++ src/app/app.datatypes.ts
@@ -1,17 +1,37 @@
 export interface Wallet {
   label: string;
-  seed: string;
   addresses: Address[];
+  seed?: string;
   balance?: number;
   hours?: number;
   hidden?: boolean;
 }
 
 export interface Address {
-  next_seed: string;
-  secret_key: string;
-  public_key: string;
   address: string;
+  next_seed?: string;
+  secret_key?: string;
+  public_key?: string;
   balance?: number;
   hours?: number;
+}
+
+export interface Output {
+  address: string;
+  coins: number;
+  hours: number;
+}
+
+export interface GetOutputsRequest {
+  head_outputs: GetOutputsRequestOutput[];
+  outgoing_outputs: any[];
+  incoming_outputs: any[];
+}
+
+export interface GetOutputsRequestOutput {
+  hash: string;
+  src_tx: string;
+  address: string;
+  coins: string;
+  hours: number;
 }
