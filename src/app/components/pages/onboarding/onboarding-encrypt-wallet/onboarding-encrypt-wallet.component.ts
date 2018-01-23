b96f26e1787a96d38bf867c720c64845123ed8ca
Jorge Remberto Rodriguez <stdev0002@simelo.tech>
2018-01-23 10:35:56 -0500

    [web wallet] refs #9 On-boarding " Encrypt your wallet" screen
    Content, styles and functions in OnboardingEncryptWalletComponent

--- src/app/components/pages/onboarding/onboarding-encrypt-wallet/onboarding-encrypt-wallet.component.ts
+++ src/app/components/pages/onboarding/onboarding-encrypt-wallet/onboarding-encrypt-wallet.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-onboarding-encrypt-wallet',
   templateUrl: './onboarding-encrypt-wallet.component.html',
-  styleUrls: ['./onboarding-encrypt-wallet.component.sass']
+  styleUrls: ['./onboarding-encrypt-wallet.component.scss']
 })
 export class OnboardingEncryptWalletComponent implements OnInit {
+  encryptWallet = false;
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.initEncryptForm();
+  }
+
+  initEncryptForm() {
+    this.form = this.formBuilder.group({
+      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
+      confirm: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
+    });
+    this.form.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.form.disable();
+  }
+
+  setEncrypt(event) {
+    this.encryptWallet = event.checked;
+    if (this.encryptWallet) {
+      this.form.enable();
+    } else {
+      this.form.disable();
+    }
+  }
+
+  onValueChanged(data) {
+
+  }
+
+  onSubmit(values) {
+
+  }
 }
