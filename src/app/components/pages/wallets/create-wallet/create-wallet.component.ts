492d02dd43c53abcaf9315aa5816ef1fd80b2c04
Jorge Remberto Rodriguez <stdev0002@simelo.tech>
2018-02-02 09:44:54 -0500

    [web wallet] refs #1 Add Wallet

--- src/app/components/pages/wallets/create-wallet/create-wallet.component.ts
+++ src/app/components/pages/wallets/create-wallet/create-wallet.component.ts
@@ -1,27 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { WalletService } from '../../../../services/wallet.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MdDialogRef } from '@angular/material';
 import * as Bip39 from 'bip39';
+import { WalletService } from '../../../../services/wallet.service';
 
 @Component({
   selector: 'app-create-wallet',
   templateUrl: './create-wallet.component.html',
-  styleUrls: ['./create-wallet.component.css']
+  styleUrls: ['./create-wallet.component.scss'],
 })
 export class CreateWalletComponent implements OnInit {
 
   form: FormGroup;
   seed: string;
+  seedValid: boolean = false;
 
-  constructor(
-    public dialogRef: MdDialogRef<CreateWalletComponent>,
-    private formBuilder: FormBuilder,
-    private walletService: WalletService,
-  ) {}
+  constructor(public dialogRef: MdDialogRef<CreateWalletComponent>,
+              private walletService: WalletService) {
+  }
 
   ngOnInit() {
     this.initForm();
+  }
+
+  closePopup() {
+    this.dialogRef.close();
   }
 
   createWallet() {
@@ -34,12 +37,20 @@ export class CreateWalletComponent implements OnInit {
   }
 
   private initForm() {
-    this.form = this.formBuilder.group({
-      label: ['', Validators.required],
-      seed: ['', Validators.required],
+    this.form = new FormGroup({});
+    this.form.addControl('label', new FormControl('', [Validators.required]));
+    this.form.addControl('seed', new FormControl('', [Validators.required]));
+    this.form.addControl('confirm_seed', new FormControl('', [
+      Validators.compose([Validators.required, this.validateAreEqual.bind(this)]),
+    ]));
+    this.form.controls.seed.valueChanges.subscribe(val => {
+      this.form.controls.confirm_seed.updateValueAndValidity({ onlySelf: true });
     });
-
     this.generateSeed();
+  }
+
+  private validateAreEqual(fieldControl: FormControl) {
+    return fieldControl.value === this.form.get('seed').value ? null : { NotEqual: true };
   }
 
 }
