a77f0f4c35aadcc01d449d489fba23bc92e8dc5d
Jorge Remberto Rodriguez <stdev0002@simelo.tech>
2018-02-12 00:23:56 -0500

    [web wallet] ref #28 Protractor test Onboarding Pages

---
+++ e2e/onboarding-create.po.ts
@@ -0,0 +1,100 @@
+import { browser, by, element } from 'protractor';
+
+export class OnboardingCreatePage {
+  navigateTo() {
+    return browser.get('/wizard/create');
+  }
+
+  getHeaderText() {
+    return element(by.css('.-header span')).getText();
+  }
+
+  getDisclaimerIsShow() {
+    return element(by.css('app-onboarding-disclaimer')).isPresent();
+  }
+
+  getSafeguardIsShow() {
+    return element(by.css('app-onboarding-safeguard')).isPresent();
+  }
+
+  acceptDisclaimer() {
+    element.all(by.css('.-disclaimer-check-text span')).first().click();
+    element(by.buttonText('Continue')).click();
+    return this.getDisclaimerIsShow();
+  }
+
+  acceptSafeguard() {
+    element.all(by.css('.mat-checkbox-label')).first().click();
+    element(by.buttonText('Continue')).click();
+    return this.getSafeguardIsShow();
+  }
+
+  getDisclaimerButtonState() {
+    return element(by.buttonText('Continue')).isEnabled();
+  }
+
+  getDisclaimerCheckedButtonState() {
+    element.all(by.css('.-disclaimer-check-text span')).first().click();
+    const button = element(by.buttonText('Continue'));
+    const state = button.getAttribute('disabled') !== null;
+    element.all(by.css('.-disclaimer-check-text span')).first().click();
+    return state;
+  }
+
+  createWallet() {
+    const btnOption = element(by.buttonText('New'));
+    btnOption.click();
+    const label = element(by.css('[formcontrolname="label"]'));
+    const seed = element(by.css('[formcontrolname="seed"]'));
+    const confirm = element(by.css('[formcontrolname="confirm_seed"]'));
+    const btnCreate = element(by.buttonText('Create'));
+
+    label.clear();
+    label.sendKeys('Test wallet');
+    seed.clear();
+    seed.sendKeys('test test');
+    confirm.clear();
+    confirm.sendKeys('test test');
+    return btnCreate.isEnabled().then(status => {
+      if (status) {
+        btnCreate.click();
+      }
+      return status;
+    });
+  }
+
+  loadWallet() {
+    const btnOption = element(by.buttonText('Load'));
+    btnOption.click();
+    const label = element(by.css('[formcontrolname="label"]'));
+    const seed = element(by.css('[formcontrolname="seed"]'));
+    const btnLoad = element(by.buttonText('Create'));
+
+    label.clear();
+    label.sendKeys('Test wallet');
+    seed.clear();
+    seed.sendKeys('test test');
+    return btnLoad.isEnabled();
+  }
+
+  skipWizard() {
+    const btnSkip = element(by.buttonText('Skip'));
+    return this.getDisclaimerIsShow().then(result => {
+      if (result) {
+        return this.acceptDisclaimer().then(res => {
+         return btnSkip.click().then(() => {
+            return browser.getCurrentUrl().then(url => {
+              return url;
+            });
+          });
+        });
+      }else {
+       return btnSkip.click().then(() => {
+          return browser.getCurrentUrl().then(url => {
+            return url;
+          });
+        });
+      }
+    });
+    }
+}
--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,3 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
-
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
@@ -19,10 +16,13 @@ exports.config = {
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  onPrepare() {
+  beforeLaunch: function() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
+  },
+  onPrepare() {
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
+
