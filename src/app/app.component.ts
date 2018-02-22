0ebb80d5caadd3bc2678137fc553bacf95edff36
Samuel Visscher <samuel@visscher.io>
2018-02-22 13:45:28 +0100

    Clean up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import 'rxjs/add/operator/takeWhile';
-import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
 import { config } from './app.config';
-import { ApiService } from './services/api.service';
-import { BlockchainService } from './services/blockchain.service';
 import { WalletService } from './services/wallet.service';
 
 @Component({
@@ -11,49 +8,20 @@ import { WalletService } from './services/wallet.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   current: number;
   highest: number;
   otcEnabled: boolean;
-  percentage: number;
   version: string;
 
   constructor(
     public walletService: WalletService,
-    private apiService: ApiService,
-    private blockchainService: BlockchainService,
   ) {
     this.otcEnabled = config.otcEnabled;
   }
 
-  ngOnInit() {
-    // this.setVersion();
-    // IntervalObservable
-    //   .create(3000)
-    //   .flatMap(() => this.blockchainService.progress())
-    //   .takeWhile(response => !response.current || response.current !== response.highest)
-    //   .subscribe(response => {
-    //     this.highest = response.highest;
-    //     this.current = response.current;
-    //     this.percentage = this.current && this.highest ? (this.current / this.highest * 100) : 0;
-    //     console.log(response);
-    //   }, error => console.log(error),
-    //     () => this.completeLoading());
-  }
-
   loading() {
     return !this.current || !this.highest || this.current !== this.highest;
-  }
-
-  private completeLoading() {
-    this.current = 999999999999;
-    this.highest = 999999999999;
-    this.walletService.refreshBalances();
-  }
-
-  private setVersion() {
-    return this.apiService.get('version')
-      .subscribe(output => this.version = output.version);
   }
 }
