a5322d542ff8c0b4d0255c7c35ad196f9bcb9ff7
Samuel Visscher <samuel@visscher.io>
2018-03-13 09:25:59 +0100

    Redesign outputs

--- src/app/components/pages/settings/outputs/outputs.component.ts
+++ src/app/components/pages/settings/outputs/outputs.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { WalletService } from '../../../../services/wallet.service';
+import { QrCodeComponent } from '../../../layout/qr-code/qr-code.component';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-outputs',
   templateUrl: './outputs.component.html',
-  styleUrls: ['./outputs.component.css']
+  styleUrls: ['./outputs.component.scss']
 })
 export class OutputsComponent implements OnInit {
 
@@ -12,9 +14,16 @@ export class OutputsComponent implements OnInit {
 
   constructor(
     public walletService: WalletService,
+    private dialog: MatDialog,
   ) { }
 
   ngOnInit() {
-    this.walletService.outputs().subscribe(outputs => this.outputs = outputs.head_outputs);
+    this.walletService.outputs().subscribe(outputs => this.outputs = outputs);
+  }
+
+  showQr(address) {
+    const config = new MatDialogConfig();
+    config.data = address;
+    this.dialog.open(QrCodeComponent, config);
   }
 }
