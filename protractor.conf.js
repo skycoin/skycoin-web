a3deaa6bf2800ee1c2f21ff403524719deb84e26
alexandermushinskiy <alexander.mushinskiy@sigma.software>
2018-04-27 18:47:35 +0300

    Set HeadlessChrome for e2e

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,7 +8,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ['--headless', '--disable-gpu']
+      args: ['no-sandbox', 'headless', 'disable-gpu']
     }
   },
   directConnect: true,
