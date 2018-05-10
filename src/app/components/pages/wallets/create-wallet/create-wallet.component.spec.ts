f8766a8079060aef3f9f3ff1a7963a0843d02f7c
SU\andrei.iarovoi <andrei.iarovoi@sigma.software>
2018-05-10 15:18:39 +0300

    #98 - Remove LoadWalletComponent: fixed e2e and unit tests

--- src/app/components/pages/wallets/create-wallet/create-wallet.component.spec.ts
+++ src/app/components/pages/wallets/create-wallet/create-wallet.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { MatDialogRef } from '@angular/material';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 import { CreateWalletComponent } from './create-wallet.component';
 import { WalletService } from '../../../../services/wallet.service';
@@ -19,6 +19,7 @@ describe('CreateWalletComponent', () => {
       schemas: [ NO_ERRORS_SCHEMA ],
       providers: [
         FormBuilder,
+        { provide: MAT_DIALOG_DATA, useValue: {} },
         { provide: WalletService, useClass: MockWalletService },
         { provide: MatDialogRef, useValue: {} }
       ]
