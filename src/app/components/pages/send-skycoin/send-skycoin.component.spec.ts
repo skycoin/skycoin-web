9e1aeae564cf929d680221141f37a8cf141e69ee
SU\andrei.iarovoi <andrei.iarovoi@sigma.software>
2018-05-31 20:36:37 +0300

    #215: refactoring of the loayout related components

--- src/app/components/pages/send-skycoin/send-skycoin.component.spec.ts
+++ src/app/components/pages/send-skycoin/send-skycoin.component.spec.ts
@@ -1,7 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
 
 import { SendSkycoinComponent } from './send-skycoin.component';
+
+@Pipe({name: 'translate'})
+class MockTranslatePipe implements PipeTransform {
+  transform() {
+    return 'translated value';
+  }
+}
 
 describe('SendSkycoinComponent', () => {
   let component: SendSkycoinComponent;
@@ -9,7 +16,7 @@ describe('SendSkycoinComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SendSkycoinComponent ],
+      declarations: [ SendSkycoinComponent, MockTranslatePipe ],
       schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
   }));
