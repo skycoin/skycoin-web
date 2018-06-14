5271d375ee64c10202bdebc25a6a61c206af0a0b
alexandermushinskiy <alexander.mushinskiy@sigma.software>
2018-06-14 16:48:25 +0300

    #294: Fix the extensive mode of the cipher test suite

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
-import { CipherTestMode } from '../app/constants/cipher-test-mode.const';
-
 export const environment = {
   production: true,
-  nodeUrl: 'https://node.skycoin.net/api/v1/',
-  cipherTestMode: CipherTestMode.NONE
+  nodeUrl: 'https://node.skycoin.net/api/v1/'
 };
