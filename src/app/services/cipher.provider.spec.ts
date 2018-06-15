e0878b16f94669c98585edc82e85c998b2198cf9
alexandermushinskiy <alexander.mushinskiy@sigma.software>
2018-06-15 15:40:06 +0300

    #304: update unit tests

--- src/app/services/cipher.provider.spec.ts
+++ src/app/services/cipher.provider.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
 
 import { CipherProvider } from './cipher.provider';
 import { Address, TransactionInput, TransactionOutput } from '../app.datatypes';
-import { convertAsciiToHexa } from '../utils/converters';
+import { CipherWebWorkerHelper } from '../utils/cipher-web-worker-helper';
 
 describe('CipherProvider', () => {
   let cipherProvider: CipherProvider;
+  const errorMessage = 'error message';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -23,29 +25,34 @@ describe('CipherProvider', () => {
 
   it('should generate address', () => {
     const expectedAddress: Address = createAddress();
-    const seed = convertAsciiToHexa('test seed');
 
-    cipherProvider.generateAddress(seed)
+    spyOn(CipherWebWorkerHelper, 'ExcecuteWorker').and.returnValue(Observable.of(createAddressInCipherFormat()));
+
+    cipherProvider.generateAddress('seed')
       .subscribe(addr => expect(addr).toEqual(expectedAddress));
   });
 
+  it('should throw error in case of any error in web worker', () => {
+    spyOn(CipherWebWorkerHelper, 'ExcecuteWorker').and.throwError(errorMessage);
+
+    expect(() => cipherProvider.generateAddress('seed'))
+      .toThrowError(errorMessage);
+  });
+
   it('should prepare transaction', () => {
-    const inputs: TransactionInput[] = [
-      createTransactionInput()
-    ];
+    const expectedRowTx = '1234567890';
 
-    const outputs: TransactionOutput[] = [
-      createTransactionOutput('2e1erPpaxNVC37PkEv3n8PESNw2DNr5aJNy'),
-      createTransactionOutput('2e1erPpaxNVC37PkEv3n8PESNw2DNr5aJNy', 20000)
-    ];
+    spyOn(CipherWebWorkerHelper, 'ExcecuteWorker').and.returnValue(Observable.of(expectedRowTx));
 
-    cipherProvider.prepareTransaction(inputs, outputs)
-      .subscribe((rowTx: string) => {
-        const actualLength = rowTx.length;
-        const expectedLength = 440;
+    cipherProvider.prepareTransaction([createTransactionInput()], [createTransactionOutput()])
+      .subscribe((rowTx: string) => expect(rowTx).toBe(expectedRowTx));
+  });
 
-        expect(actualLength).toBe(expectedLength);
-      });
+  it('should throw error in case of any error in web worker', () => {
+    spyOn(CipherWebWorkerHelper, 'ExcecuteWorker').and.throwError(errorMessage);
+
+    expect(() => cipherProvider.prepareTransaction([createTransactionInput()], [createTransactionOutput()]))
+      .toThrowError(errorMessage);
   });
 });
 
@@ -65,10 +72,19 @@ function createTransactionInput(): TransactionInput {
   };
 }
 
-function createTransactionOutput(address: string, coins = 10000, hours = 0): TransactionOutput {
+function createTransactionOutput(address = '2e1erPpaxNVC37PkEv3n8PESNw2DNr5aJNy', coins = 10000, hours = 0): TransactionOutput {
   return {
     address: address,
     coins: coins,
     hours: hours
+  };
+}
+
+function createAddressInCipherFormat() {
+  return {
+    NextSeed: '9fe8bfb01de85dbba36cbd9854ad7478cd63459fedb4c9f7847bf280ee17a32c',
+    Secret: '20c3db0e1f3b95d98d1f78d73c134af8f1b5dd34cc05f053da94c20d72558862',
+    Public: '030a797a31100d3a7b5b403f551975e9a12f93b4d4e9e44b402b84832e0c7b89d2',
+    Address: '2uATq4pdSb8Ka1YKSAAbp6Npehs3QQqTnb'
   };
 }
