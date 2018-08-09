a4e9595ea585fd6cb239136e2ac575be9a57f36a
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2018-08-09 17:54:54 -0400

    Start integrating with Electron

---
+++ electron/src/electron-main.js
@@ -0,0 +1,209 @@
+'use strict'
+
+const { app, Menu, BrowserWindow, shell, session, protocol } = require('electron');
+const path = require('path')
+
+const uiDir = path.join(__dirname, './dist/');
+
+// This adds refresh and devtools console keybindings
+// Page can refresh with cmd+r, ctrl+r, F5
+// Devtools can be toggled with cmd+alt+i, ctrl+shift+i, F12
+require('electron-debug')({enabled: true, showDevTools: false});
+require('electron-context-menu')({});
+
+global.eval = function() { throw new Error('bad!!'); }
+
+// Force everything localhost, in case of a leak
+app.commandLine.appendSwitch('host-rules', 'MAP * 127.0.0.1, EXCLUDE api.coinmarketcap.com, api.github.com');
+app.commandLine.appendSwitch('ssl-version-fallback-min', 'tls1.2');
+app.commandLine.appendSwitch('--no-proxy-server');
+app.setAsDefaultProtocolClient('skycoin');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  // Fix requests made outside the dist folder.
+  protocol.interceptFileProtocol('file', (request, callback) => {
+    var filePath = request.url.substr(7)
+    switch (process.platform) {
+      case 'win32':
+        filePath = filePath.startsWith('/') || filePath.startsWith('\\') ? filePath.substr(1) : filePath;
+    }
+    filePath = path.normalize(filePath);
+
+    const changePos = findFirstChangePos(uiDir, filePath);
+    filePath = changePos == -1 ?
+      filePath :
+      filePath.substr(0, changePos) + uiDir.substr(changePos) +  filePath.substr(changePos);
+
+    callback({path: filePath})
+  }, (error) => {
+    if (error) console.error('Failed to register protocol')
+  });
+
+  createWindow();
+});
+
+function createWindow() {
+  console.log('Creating window');
+
+  // To fix appImage doesn't show icon in dock issue.
+  var appPath = app.getPath('exe');
+  var iconPath = (() => {
+    switch (process.platform) {
+      case 'linux':
+        return path.join(path.dirname(appPath), './resources/icon512x512.png');
+    }
+  })()
+
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    title: 'Skycoin',
+    icon: iconPath,
+    nodeIntegration: false,
+    webPreferences: {
+      webgl: false,
+      webaudio: false,
+      contextIsolation: true,
+      webviewTag: false,
+      nodeIntegration: false,
+      nodeIntegrationInWorker: false,
+      allowRunningInsecureContent: false,
+      webSecurity: true,
+      plugins: false,
+    },
+  });
+
+  // patch out eval
+  win.eval = global.eval;
+  win.webContents.executeJavaScript('window.eval = 0;');
+
+  win.loadURL(`file://${uiDir}index.html`);
+
+  const ses = win.webContents.session
+  ses.clearCache(function () {
+    console.log('Cleared the caching of the skycoin wallet.');
+  });
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+
+  win.webContents.on('will-navigate', function(e, url) {
+    e.preventDefault();
+    require('electron').shell.openExternal(url);
+  });
+
+  // create application's main menu
+  var template = [{
+    label: 'Skycoin',
+    submenu: [
+      { label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); } }
+    ]
+  }, {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+      { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+      { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+      { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+    ]
+  }, {
+    label: 'Show',
+    submenu: [
+      {
+        label: 'DevTools',
+        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            focusedWindow.toggleDevTools();
+          }
+        }
+      },
+    ]
+  }];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+  session
+    .fromPartition('')
+    .setPermissionRequestHandler((webContents, permission, callback) => {
+      return callback(false);
+    });
+}
+
+// Get the index of the first character from which two string are no longer equal
+function findFirstChangePos(stringA, stringB) {
+  var limit = Math.min(stringA.length, stringB.length);
+  for (var i = 0; i < limit; i++) {
+    if (stringA[i] !== stringB[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// Enforce single instance
+const alreadyRunning = app.makeSingleInstance((commandLine, workingDirectory) => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (win) {
+    if (win.isMinimized()) {
+      win.restore();
+    }
+    win.focus();
+  } else {
+    createWindow();
+  }
+});
+
+if (alreadyRunning) {
+  app.quit();
+  return;
+}
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
+
+app.on('web-contents-created', (event, contents) => {
+  contents.on('will-attach-webview', (event, webPreferences, params) => {
+    // Strip away preload scripts if unused or verify their location is legitimate
+    delete webPreferences.preload
+    delete webPreferences.preloadURL
+
+    // Disable Node.js integration
+    webPreferences.nodeIntegration = false
+
+    // Verify URL being loaded
+    if (!params.src.startsWith('file:')) {
+      event.preventDefault();
+    }
+  });
+});
