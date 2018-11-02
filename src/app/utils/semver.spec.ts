54e2005c63bfe0ff8f293a3a1bc07414081de26c
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2018-11-02 08:06:24 -0400

    Change the way of comparing versions

--- src/app/utils/semver.spec.ts
+++ src/app/utils/semver.spec.ts
@@ -4,14 +4,11 @@ describe('semver', () => {
   it('correctly compares versions', () => {
     expect(isEqualOrSuperiorVersion('0.23.0', '0.22.0')).toBeTruthy();
     expect(isEqualOrSuperiorVersion('0.23.0', '0.23.0')).toBeTruthy();
-    expect(isEqualOrSuperiorVersion('0.23.0', '0.23.1')).toBeFalsy();
+    expect(isEqualOrSuperiorVersion('0.23.0', '0.23.1')).toBeTruthy();
     expect(isEqualOrSuperiorVersion('0.23.1', '0.24.0')).toBeFalsy();
     expect(isEqualOrSuperiorVersion('0.24.0', '1.0.0')).toBeFalsy();
-  });
-
-  it('correctly handles rc versions', () => {
-    expect(isEqualOrSuperiorVersion('0.23.1-rc.1', '0.23.0')).toBeTruthy();
-    expect(isEqualOrSuperiorVersion('0.23.1-rc.1', '0.23.1')).toBeTruthy();
-    expect(isEqualOrSuperiorVersion('0.23.1-rc.1', '0.23.2')).toBeFalsy();
+    expect(isEqualOrSuperiorVersion('0.24', '0.23')).toBeTruthy();
+    expect(isEqualOrSuperiorVersion('0.24', '0.24')).toBeTruthy();
+    expect(isEqualOrSuperiorVersion('0.24', '0.25')).toBeFalsy();
   });
 });
--- src/app/utils/semver.ts
+++ src/app/utils/semver.ts
@@ -1,16 +1,16 @@
 /**
- * Return true is current is equal or superior to target. The -rc parts are ignored.
+ * Return true is current is equal or superior to target. Versions normally are composed of 3 parts,
+ * separated by points, but this function only takes into account the first two parts, so 0.24.0 is
+ * considered equal to 0.24.1.
  */
 export function isEqualOrSuperiorVersion(current: string, target: string): boolean {
   const currentParts = current.split('.');
-  if (currentParts.length < 3) { return false; }
-  currentParts[2] = currentParts[2].split('-')[0];
+  if (currentParts.length < 2) { return false; }
 
   const targetParts = target.split('.');
-  if (targetParts.length < 3) { return false; }
-  targetParts[2] = targetParts[2].split('-')[0];
+  if (targetParts.length < 2) { return false; }
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 2; i++) {
     const currentNumber = Number(currentParts[i]);
     const targetNumber = Number(targetParts[i]);
 
