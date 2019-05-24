b5408bd847d5b3c9c89294ec183eadbceece2289
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2019-05-23 20:54:45 -0400

    Fix unit tests

--- src/app/components/pages/send-skycoin/send-form/send-form.component.spec.ts
+++ src/app/components/pages/send-skycoin/send-form/send-form.component.spec.ts
@@ -8,9 +8,10 @@ import { WalletService } from '../../../../services/wallet/wallet.service';
 import { SpendingService } from '../../../../services/wallet/spending.service';
 import { CoinService } from '../../../../services/coin.service';
 import { BlockchainService } from '../../../../services/blockchain.service';
-import { MockTranslatePipe, MockWalletService, MockSpendingService, MockCoinService, MockBlockchainService, MockCustomMatDialogService, MockNavBarService } from '../../../../utils/test-mocks';
+import { MockTranslatePipe, MockWalletService, MockSpendingService, MockCoinService, MockBlockchainService, MockCustomMatDialogService, MockNavBarService, MockPriceService } from '../../../../utils/test-mocks';
 import { CustomMatDialogService } from '../../../../services/custom-mat-dialog.service';
 import { NavBarService } from '../../../../services/nav-bar.service';
+import { PriceService } from '../../../../services/price.service';
 
 describe('SendFormComponent', () => {
   let component: SendFormComponent;
@@ -29,6 +30,7 @@ describe('SendFormComponent', () => {
         { provide: BlockchainService, useClass: MockBlockchainService },
         { provide: CustomMatDialogService, useClass: MockCustomMatDialogService },
         { provide: NavBarService, useClass: MockNavBarService },
+        { provide: PriceService, useClass: MockPriceService },
       ]
     }).compileComponents();
   }));
