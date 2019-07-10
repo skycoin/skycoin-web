ec9848fa9ad208a31d2022ffb38ac7ba66be8f50
iketheadore <2589926+iketheadore@users.noreply.github.com>
2019-07-10 19:04:58 +1000

    Add script to do electron build

---
+++ electron/build-electron-release.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+
+if [[ -n "$1" ]]; then
+  OSARCH="$@"
+else
+  OSARCH="linux/amd64 windows/amd64 darwin/amd64"
+fi
+
+# Get build version from package.json
+APP_VERSION=`grep version package.json | sed 's/[,\", ]//g' | awk '{split($0,a,":");print a[2]}'`
+
+# package name
+PKG_NAME=`grep productName package.json | sed 's/[,\", ]//g' | awk '{split($0,s,":");print tolower(s[2])}'`
+
+# product name
+PDT_NAME=`grep productName package.json | sed 's/[,\", ]//g' | awk '{split($0,s,":");print s[2]}'`
+
+if [[ "$OSARCH" == *"darwin/amd64"* ]]; then
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    ./build-server.sh "darwin/amd64"
+    npm run dist-mac
+  else
+    echo "Can not run build script in $OSTYPE"
+  fi
+fi
+
+if [[ "$OSARCH" == *"windows/amd64"* ]]; then
+  ./build-server.sh "windows/amd64"
+  npm run dist-win
+fi
+
+if [[ "$OSARCH" == *"linux/amd64"* ]]; then
+  ./build-server.sh "linux/amd64"
+  npm run dist-linux
+fi
+
+pushd "release" >/dev/null
+
+# rename linux AppImage file
+IMG="${PKG_NAME}-${APP_VERSION}-x86_64.AppImage"
+DEST_IMG="${PKG_NAME}-${APP_VERSION}-gui-electron-linux-x64.AppImage"
+if [[ -e "$IMG" ]]; then
+  mv "$IMG" "$DEST_IMG"
+  chmod +x "$DEST_IMG"
+fi
+
+# rename windows setup file
+EXE="${PDT_NAME} Setup ${APP_VERSION}.exe"
+if [[ -e "$EXE" ]]; then
+  mv "$EXE" "${PKG_NAME}-${APP_VERSION}-gui-electron-win-setup.exe"
+fi
+
+# rename dmg file
+DMG="${PKG_NAME}-${APP_VERSION}.dmg"
+if [[ -e "$DMG" ]]; then
+  mv "$DMG" "${PKG_NAME}-${APP_VERSION}-gui-electron-osx.dmg"
+fi
+
+# delete mac folder
+if [[ -d "mac" ]]; then
+  rm -rf mac
+fi
+
+SNAP="${PKG_NAME}_${APP_VERSION}_amd64.snap"
+if [[ -e "$SNAP" ]]; then
+  rm -f "$SNAP"
+fi
+
+# delete app zip file
+MZIP="${PKG_NAME}-${APP_VERSION}-mac.zip"
+if [[ -e "$MZIP" ]]; then
+  rm "$MZIP"
+fi
+
+# clean unpacked folders
+rm -rf *-unpacked
+
+# delete blockmap and electron-builder.yaml
+rm -f *.blockmap
+rm -f *.yaml
+
+popd >/dev/null
--- electron/build-server.sh
+++ electron/build-server.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-echo "Compiling for win_ia32"
-env GOOS=windows GOARCH=386 go build -o ./server/win_ia32/server.exe
-echo "Compiling for win_x64"
-env GOOS=windows GOARCH=amd64 go build -o ./server/win_x64/server.exe
-echo "Compiling for linux_x64"
-env GOOS=linux GOARCH=amd64 go build -o ./server/linux_x64/server
-echo "Compiling for mac_x64"
-env GOOS=darwin GOARCH=amd64 go build -o ./server/mac_x64/server
+if [[ -n "$1" ]]; then
+  OSARCH="$@"
+else
+  OSARCH="linux/amd64 windows/amd64 windows/386 darwin/amd64"
+fi
+
+if [[ "$OSARCH" == *"windows/386"* ]]; then
+  echo "Compiling for win_ia32"
+  env GOOS=windows GOARCH=386 go build -o ./server/win_ia32/server.exe
+fi
+
+
+if [[ "$OSARCH" == *"windows/amd64"* ]]; then
+  echo "Compiling for win_x64"
+  env GOOS=windows GOARCH=amd64 go build -o ./server/win_x64/server.exe
+fi
+
+if [[ "$OSARCH" == *"linux/amd64"* ]]; then
+  echo "Compiling for linux_x64"
+  env GOOS=linux GOARCH=amd64 go build -o ./server/linux_x64/server
+fi
+
+if [[ "$OSARCH" == *"darwin/amd64"* ]]; then
+  echo "Compiling for mac_x64"
+  env GOOS=darwin GOARCH=amd64 go build -o ./server/mac_x64/server
+fi
